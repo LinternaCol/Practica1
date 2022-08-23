b355721af8dd499b5615b5259a9fb3b6dba47435
DESKTOP-Q94H06I\JuanSuarez <juanpablosuarezperdomo1@gmail.com>
2022-08-22 22:10:04 -0500

    Proyecto terminado para salir a produccion.

--- Project1/src/main/java/com/registro/usuarios/RegistroUsuariosSpringSecurityApplication.java
+++ Project1/src/main/java/com/registro/usuarios/RegistroUsuariosSpringSecurityApplication.java
@@ -1,13 +1,13 @@
-package com.example.demo;
+package com.registro.usuarios;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Project1Application {
+public class RegistroUsuariosSpringSecurityApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Project1Application.class, args);
+		SpringApplication.run(RegistroUsuariosSpringSecurityApplication.class, args);
 	}
 
 }
